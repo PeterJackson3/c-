9a05dd9e32dc0297ae9788520b714b4dcee4ac0a
PeterJackson3 <167114995+PeterJackson3@users.noreply.github.com>
2024-08-20 05:50:54 +0000

    code

---
+++ news.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!"<<endl;
+  cout << "Hello dumb!"<<endl;
+  int num=90;
+  int nums=100;
+  int test=nums+num;
+  cout<<test<<endl;
+
+
+  return 0;
+}
